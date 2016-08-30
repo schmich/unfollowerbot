d4cbe44b2855342f0dbffa7e155586d0bc255edd
Chris Schmich <schmch@gmail.com>
2016-08-30 10:19:12 -0500

    Remove mongo requirement. Use raw files for storage.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,5 @@
 source 'https://rubygems.org'
 
-gem 'mongo'
 gem 'mail'
 gem 'tzinfo'
 gem 'tzinfo-data'
--- snapshot.rb
+++ snapshot.rb
@@ -1,22 +1,19 @@
 require 'open-uri'
 require 'json'
-require 'mongo'
 require 'set'
 require 'mail'
 require 'erb'
 require 'tzinfo'
 require 'logger'
-
-$log = Logger.new(STDOUT)
-$log.level = Logger::DEBUG
+require 'fileutils'
 
 class Twitch
-  def self.followers(username)
+  def self.followers(channel)
     names = Set.new
 
     last_request = Time.now
 
-    follows_urls(username) do |url|
+    follows_urls(channel) do |url|
       # Sleep 1 second between requests.
       now = Time.now
       delta = now - last_request
@@ -34,10 +31,10 @@ class Twitch
     names.to_a
   end
 
-  def self.follows_urls(username)
+  def self.follows_urls(channel)
     offset = 0
     loop do
-      yield "https://api.twitch.tv/kraken/channels/#{username}/follows?limit=100&offset=#{offset}&direction=ASC"
+      yield "https://api.twitch.tv/kraken/channels/#{channel}/follows?limit=100&offset=#{offset}&direction=ASC"
       offset += 80
     end
   end
@@ -71,37 +68,47 @@ class Diff
 end
 
 class Snapshot
-  def initialize(collection)
-    @collection = collection
+  def Snapshot.create(channel)
+    $log.info "Creating snapshot for #{channel}."
+    followers = Twitch.followers(channel)
+    Snapshot.new(channel, followers)
   end
 
-  def save(username)
-    followers = Twitch.followers(username)
-
-    snapshot = {
-      channel: username,
-      timestamp: Time.now.utc,
-      followers: followers
-    }
-
-    @collection.insert(snapshot)
+  def Snapshot.load(filename)
+    File.open(filename, 'r') do |f|
+      obj = JSON.parse(f.read)
+      timestamp = Time.parse(obj['timestamp'])
+      return Snapshot.new(obj['channel'], obj['followers'], timestamp)
+    end
   end
 
-  def recent_snapshots(username)
-    docs = @collection.find({ channel: /^#{username}$/i }).sort({ timestamp: -1 }).limit(2).to_a
-    after, before = docs
-    [before, after]
+  def save(filename)
+    File.open(filename, 'w') do |f|
+      snapshot = {
+        channel: @channel,
+        timestamp: @timestamp,
+        followers: @followers
+      }
+      f.write JSON.dump(snapshot)
+    end
+  end
+
+  attr_accessor :channel, :followers, :timestamp
+
+  private
+
+  def initialize(channel, followers, timestamp = Time.now.utc)
+    @channel = channel
+    @followers = followers
+    @timestamp = timestamp
   end
 end
 
 class Report
   def initialize(before, after)
-    before_followers = before['followers']
-    after_followers = after['followers']
-
     @before = before
     @after = after
-    @diff = Diff.new(before_followers, after_followers)
+    @diff = Diff.new(before.followers, after.followers)
   end
 
   def before
@@ -120,6 +127,32 @@ class Report
     @diff.added
   end
 
+  def email(emails)
+    Mail.defaults do
+      delivery_method :smtp, {
+        address: 'smtp.gmail.com',
+        port: 587,
+        user_name: 'unfollowerbot@gmail.com',
+        password: ENV['UNFOLLOWERBOT_EMAIL_PASSWORD'],
+        authentication: 'plain',
+        enable_starttls_auto: true
+      }
+    end
+
+    Mail.deliver do
+      to emails
+      from 'unfollowerbot <unfollowerbot@gmail.com>'
+      subject "Twitch follower report for #{Time.now.strftime('%m/%d')}"
+
+      html_part do
+        content_type 'text/html; charset=UTF-8'
+        body self.html
+      end
+    end
+  end
+
+  private
+
   def html
     template_path = File.absolute_path(File.join(File.dirname(__FILE__), 'report.html.erb'))
     template = ERB.new(File.read(template_path))
@@ -127,55 +160,54 @@ class Report
   end
 end
 
-def mail_report(emails, report)
-  Mail.defaults do
-    delivery_method :smtp, {
-      address: 'smtp.gmail.com',
-      port: 587,
-      user_name: 'unfollowerbot@gmail.com',
-      password: ENV['UNFOLLOWERBOT_EMAIL_PASSWORD'],
-      authentication: 'plain',
-      enable_starttls_auto: true
-    }
+class SnapshotReportManager
+  def initialize(snapshot_dir)
+    @snapshot_dir = snapshot_dir
   end
 
-  Mail.deliver do
-    to emails
-    from 'unfollowerbot <unfollowerbot@gmail.com>'
-    subject "Twitch follower report for #{Time.now.strftime('%m/%d')}"
+  def update(channel, emails)
+    FileUtils.mkdir_p(@snapshot_dir)
+    snapshot_filename = "#{@snapshot_dir}/#{channel.downcase}.json"
 
-    html_part do
-      content_type 'text/html; charset=UTF-8'
-      body report.html
+    begin
+      $log.info 'Loading previous snapshot.'
+      before = Snapshot.load(snapshot_filename)
+    rescue Errno::ENOENT
+      $log.info 'Previous snapshot not found.'
     end
+
+    after = Snapshot.create(channel)
+
+    if before && after
+      report = Report.new(before, after)
+      if report.removed.empty? && report.added.empty?
+        $log.info 'No new followers or unfollowers, not sending report.'
+        return
+      else
+        $log.info "Sending report to #{emails.join(', ')} for #{channel}."
+        report.email(emails)
+      end
+    else
+      $log.info 'No snapshot to compare with, not sending report.'
+    end
+
+    after.save(snapshot_filename)
   end
 end
 
-def snapshot_report(username, emails)
-  mongo = Mongo::MongoClient.new('127.0.0.1')
-  db = mongo.db('unfollowerbot')
-  collection = db.collection('snapshots')
+$log = Logger.new(STDOUT)
+$log.level = Logger::DEBUG
 
-  $log.info "Taking snapshot for #{username}."
-  snapshot = Snapshot.new(collection)
-  snapshot.save(username)
-
-  $log.info "Fetching recent snapshots for #{username}."
-  before, after = snapshot.recent_snapshots(username)
-  return if !before || !after
-
-  $log.info "Sending report to #{emails.join(', ')} for #{username}."
-  report = Report.new(before, after)
-  mail_report(emails, report)
-end
-
-username = ARGV[0]
+channel = ARGV[0]
 emails = ARGV[1]
 
-if !username || !emails
-  $log.error 'Usage: snapshot <twitch username> <emails>'
+if !channel || !emails
+  $log.error 'Usage: snapshot <twitch channel> <emails>'
   exit 1
 end
 
-snapshot_report(username, emails.split(';'))
+$log.info "Doing snapshot update for #{channel}."
+snapshot_dir = File.absolute_path(File.join(File.dirname(__FILE__), 'snapshots'))
+manager = SnapshotReportManager.new(snapshot_dir)
+manager.update(channel, emails.split(';'))
 $log.info 'Fin.'
